3c35ecb7841e77d263217318d7458c0d97068716
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-09-18 18:21:39 +0200

    add css, js and html sample

--- text-editor.js
+++ text-editor.js
@@ -1,6 +1,7 @@
 module.exports = function(app){
+    var pell = require('pell');
     var TextEditor = Object.getPrototypeOf(app).TextEditor = new app.Component("text-editor");
-    //TextEditor.debug = true;
+    TextEditor.debug = true;
     TextEditor.createdAt      = "2.0.0";
     TextEditor.lastUpdate     = "2.0.0";
     TextEditor.version        = "1";
@@ -8,8 +9,52 @@ module.exports = function(app){
     // TextEditor.loadingMsg     = "This message will display in the console when component will be loaded.";
     // TextEditor.requires       = [];
 
-    // TextEditor.prototype.onCreate = function(){
-    // do thing after element's creation
-    // }
+    TextEditor.prototype.onCreate = function(){
+        var editor = this;
+        editor.$el.wrap('<div class="text-editor__wrapper"></div>');
+        editor.$input = $('<div class="text-editor__input"></div>').insertAfter(editor.$el);
+        editor.pell = pell.init({
+            element: editor.$input[0],
+            onChange: function(html){
+                editor.$el.val(html);
+            },
+            classes:{
+            // actionbar: 'pell-actionbar',
+            // button: 'pell-button',
+            // content: 'pell-content',
+            // selected: 'pell-button-selected'
+            },
+            defaultParagraphSeparator: 'p',
+            actions:[
+                'bold',
+                'italic',
+                'underline',
+                {
+                    name: 'olist',
+                    icon: '<i class="fas fa-list-ol"></i>',
+                },
+                {
+                    name: 'ulist',
+                    icon: '<i class="fas fa-list-ul"></i>',
+                },
+                'line',
+                {
+                    name: 'link',
+                    icon: '<i class="fas fa-link"></i>',
+                    result: () => {
+                        var url = window.prompt('Indiquez l\'URL du lien');
+                        var text = document.getSelection();
+                        if (url) {
+                            var target = window.confirm('Ouvrir le lien dans une autre fenêtre ?');
+                            target ? document.execCommand('insertHTML', false, '<a target="_blank" href="' + url + '">' + text + '</a>') : document.execCommand('createLink', false, url);
+                        }
+                    }
+                },
+            ],
+        });
+        editor.$el.on('change keyup',function(){
+            editor.pell.content.innerHTML = editor.$el.val();
+        }).trigger('change');
+    }
     return TextEditor;
 }
